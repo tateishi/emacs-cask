47edcb6ba295eabe3d44a0810b710677af981795
Tateishi Tadatoshi <ishio39@gmail.com>
2021-04-09 20:11:29 +0900

    kinshu-mode の変更

--- lisp/kinshu-mode.el
+++ lisp/kinshu-mode.el
@@ -1,15 +1,60 @@
-;; kinshu-mode.el  -*- lexical-binding: t; -*-
+;;; kinshu-mode.el --- Major mode for editing kinshu files. -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021 TATEISHI Tadatoshi
+
+;; Author: TATEISHI Tadatoshi <ishio39@gmail.com>
+;; Maintainer: TATEISHI Tadatoshi <ishio39@gmail.com>
+;; Created: 2021/04/09
+;; Version: 0.0.1
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;;; Code:
 
 (require 'cl-lib)
 (require 'seq)
 
-(defvar kinshu-list
+(defconst kinshu-list
   '(10000 5000 2000 1000 500 100 50 10 5 1))
 
-(defvar kinshu-tabs
+(defconst kinshu-tabs
   '(0 13 17 21 25 29 33 37 41 45 49))
 
-(defun kinshu (count-list)
+(defconst kinshu-header
+  (concat
+   "#-------------------------------------------------------------\n"
+   "#kinshu    10K  5K  2K  1K 500 100  50  10   5   1         sum\n"
+   "#-------------------------------------------------------------\n"))
+
+(defconst kinshu-line-template
+  "%s   0   0   0   0   0   0   0   0   0   0")
+
+(defconst kinshu-date-template
+  "%Y/%m/%d")
+
+(defconst kinshu-sum-template
+  "   =%8d")
+
+(defun kinshu-amount (count-list)
   (apply #'+ (cl-mapcar #'* kinshu-list count-list)))
 
 (defun kinshu-read-counts (from)
@@ -34,11 +79,17 @@
         from
       (seq-min next-list))))
 
-(defun before-number (p)
+(defun kinshu-prev(from tabs)
+  (let ((next-list (seq-filter (lambda (m) (< m from)) tabs)))
+    (if (null next-list)
+        from
+      (seq-max next-list))))
+
+(defun before-number ()
   (skip-chars-backward "0-9")
   (point))
 
-(defun after-number (p)
+(defun after-number ()
   (skip-chars-forward "0-9")
   (point))
 
@@ -47,8 +98,8 @@
   (save-excursion
     (let ((ch (char-after)))
       (when (and (>= ch ?0) (<= ch ?9))
-          (let* ((from (before-number (point)))
-                 (to (after-number (point)))
+          (let* ((from (before-number))
+                 (to (after-number))
                  (num (1+ (string-to-number (buffer-substring from to))))
                  (len (min (length (format "%d" num)) (- to from))))
             (delete-char (- len))
@@ -59,8 +110,8 @@
   (save-excursion
     (let ((ch (char-after)))
       (when (and (>= ch ?0) (<= ch ?9))
-          (let* ((from (before-number (point)))
-                 (to (after-number (point)))
+          (let* ((from (before-number))
+                 (to (after-number))
                  (num (1- (string-to-number (buffer-substring from to))))
                  (len (max (length (format "%d" num)) (- to from))))
             (delete-char (- len))
@@ -71,8 +122,9 @@
   (interactive)
   (save-excursion
     (kinshu-delete-sum)
-    (end-of-line)
-    (insert (format "   =%8d" (kinshu (kinshu-read-counts (current-buffer)))))))
+    (goto-char (point-max))
+    (let ((amount (kinshu-amount (kinshu-read-counts (current-buffer)))))
+      (insert (format kinshu-sum-template amount)))))
 
 (defun kinshu-sum-region (min max)
   "一行分の金種からの合計金額計算をリージョンに対して行う"
@@ -86,25 +138,30 @@
 (defun kinshu-add-header ()
   "金種計算のヘッダーを追加する"
   (interactive)
-  (end-of-buffer)
-  (insert "#-------------------------------------------------------------\n")
-  (insert "#kinshu    10K  5K  2K  1K 500 100  50  10   5   1         sum\n")
-  (insert "#-------------------------------------------------------------\n"))
+  (goto-char (point-max))
+  (insert kinshu-header))
 
 (defun kinshu-add-line ()
   "金種計算の行を追加する"
   (interactive)
-  (end-of-buffer)
-  (let ((today (format-time-string "%Y/%m/%d")))
-    (insert (format "%s   0   0   0   0   0   0   0   0   0   0" today)))
+  (goto-char (point-max))
+  (let ((today (format-time-string kinshu-date-template)))
+    (insert (format kinshu-line-template today)))
   (beginning-of-line))
 
 (defun kinshu-next-tab ()
-  "金種のフォーマットに合わせてカーソル移動する"
+  "金種のフォーマットに合わせてカーソルを右に移動する"
   (interactive)
   (let* ((col (current-column))
          (next (kinshu-next col kinshu-tabs)))
     (move-to-column next t)))
+
+(defun kinshu-prev-tab ()
+  "金種のフォーマットに合わせてカーソルを左に移動する"
+  (interactive)
+  (let* ((col (current-column))
+         (prev (kinshu-prev col kinshu-tabs)))
+    (move-to-column prev t)))
 
 (defvar kinshu-mode-map
   (let ((map (make-sparse-keymap)))
@@ -117,6 +174,8 @@
     (define-key map (kbd "-") #'kinshu-dec)
     (define-key map (kbd "_") #'kinshu-dec)
     (define-key map (kbd "C-i") #'kinshu-next-tab)
+    (define-key map (kbd "M-f") #'kinshu-next-tab)
+    (define-key map (kbd "M-b") #'kinshu-prev-tab)
     map))
 
 (autoload 'text-mode "text-mode")
@@ -128,7 +187,4 @@
 
 (provide 'kinshu-mode)
 
-;; Local Variables:
-;; mode: emacs-lisp
-;; coding: utf-8
-;; End:
+;;; kinshu-mode.el ends here
