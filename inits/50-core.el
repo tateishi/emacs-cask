619a4d0f9450944900fe832d8c5e0f7c611fdfd1
Tateishi Tadatoshi <tateishi@tw.tokai-tv.co.jp>
2021-04-12 13:06:53 +0900

    add comment

--- inits/50-core.el
+++ inits/50-core.el
@@ -1,4 +1,32 @@
-;;;; 50-core.el -*- lexical-binding: t -*-
+;;; 50-core.el --- My init core  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021 TATEISHI Tadatoshi
+
+;; Author: TATEISHI Tadatoshi <ishio39@gmail.com>
+;; Created: 2021/04/12
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; My init core.
+
+;;; Code:
 
 ;;;
 ;;; general setting
@@ -307,3 +335,5 @@ _q_: exit
   :load-path "lisp"
   :commands (kinshu-mode)
   :hook (kinshu-mode . my-kinshu-mode-hook))
+
+;;; 50-core.el ends here
