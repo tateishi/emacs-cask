363c934bd549c532aa9f7814a3b79ffe7cb50202
Tateishi Tadatoshi <ishio39@gmail.com>
2022-04-08 01:56:43 +0900

    fix load-path

--- init.el
+++ init.el
@@ -11,6 +11,16 @@
 (if (file-exists-p custom-file) (load custom-file))
 
 
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths)
+      (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
+	(add-to-list 'load-path default-directory)
+	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+	    (normal-top-level-add-subdirs-to-load-path))))))
+
+(add-to-load-path ".cask/28.1/elpa")
+
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 
